91903ffafa575a83c5d2775a81e44f6f7f44116c
Matthew Chang <matthew.chang@tufts.edu>
2019-05-29 18:01:16 -0400

    Added faster shuffle algorithm

--- tensor_flow_stuff/iris_classifier/iris_classifier.py
+++ tensor_flow_stuff/iris_classifier/iris_classifier.py
@@ -14,11 +14,20 @@ def scale_data(arr):
     arr[:, 3] = (arr[:, 3] - 0.1) / 2.4
 
 
+# fastest way to shuffle 150 samples - 0.04758 ms per loop
+def shuffle_data(arr):
+    # only swap within each class to ensure same # of samples in training
+    for i in range(3):
+        # generate random numbers and sort the indices
+        idx = np.argsort(np.random.random(50)) + 50 * i
+        arr[i * 50:(i + 1) * 50, :] = arr[idx, :]
+
+
 # preallocate matrix for iris data and labels
 data = np.empty((150, 4))
 labels = np.empty(150)
-for i in range(3):
-    labels[50*i:50*(i+1)].fill(i)
+for n in range(3):
+    labels[50*n:50*(n+1)].fill(n)
 
 # define classes
 iris_classes = ['Setosa', 'Versicolour', 'Virginica']
@@ -33,18 +42,7 @@ with open('A4 - Iris data.txt') as f:
 # preprocess data: normalize data to fall between 0 and 1
 scale_data(data)
 
-# mix up data
-for _ in range(100):
-    # only swap within each class to ensure same # of samples in training
-    for j in range(3):
-        a, b = random.sample(range(j * 50, (j + 1) * 50), 2)
-        # # slower swap - advanced indexing: extracts rows a & b as 2D arr
-        # data[[a, b]] = data[[b, a]]
-
-        # faster swapping method
-        copy = data[a, :].copy()
-        data[a, :], data[b, :] = data[b, :], copy
-        labels[a], labels[b] = labels[b], labels[a]
+shuffle_data(data)
 
 # portion data for different purposes
 training_data = np.vstack((data[0:40], data[50:90], data[100:140]))
@@ -77,3 +75,40 @@ test_loss, test_acc = model.evaluate(test_data, test_labels)
 print('Test Accuracy:', test_acc)
 
 predictions = model.predict(test_data)
+
+
+# --- APPENDIX ---
+
+# Fisher-Yates shuffle: intuitive but slowest - 2.58197 ms per loop
+def shuffle_data_v1(arr):
+    for _ in range(100):
+        # only swap within each class to ensure same # of samples in training
+        for j in range(3):
+            a, b = random.sample(range(j * 50, (j + 1) * 50), 2)
+            # # slower swap - advanced indexing: extracts rows a & b as 2D arr
+            # data[[a, b]] = data[[b, a]]
+
+            # faster swapping method
+            copy = arr[a, :].copy()
+            arr[a, :], arr[b, :] = arr[b, :], copy
+
+
+# shuffle data directly: slower than shuffling index - 0.21279 ms per loop
+def shuffle_data_v2(arr):
+    for i in range(3):
+        np.random.shuffle(arr[i * 50:(i + 1) * 50])
+
+
+# shuffle indices and reassign: slower than argsort index - 0.17388 ms per loop
+def shuffle_data_v3(arr):
+    for i in range(3):
+        idx = np.arange(i * 50, (i + 1) * 50)
+        np.random.shuffle(idx)
+        arr[i * 50:(i + 1) * 50, :] = arr[idx, :]
+
+
+# permutate indices and reassign: slower than argsort index - 0.18144 ms per loop
+def shuffle_data_v4(arr):
+    for i in range(3):
+        idx = np.random.permutation(50) + 50 * i
+        arr[i * 50:(i + 1) * 50, :] = arr[idx, :]
